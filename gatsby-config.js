d2b7d82d969e5c4ea1b5ea1269cad4387440729c
Edward Baldacchino <edward.baldacchino@live.vu.edu.au>
2021-03-18 13:01:25 +1100

    Added Facebook Card meta tags

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,8 +33,7 @@ module.exports = {
     {
       resolve: `gatsby-source-contentful`,
       options: {
-        spaceId: `rb3nh7syexm7`,
-        // Learn about environment variables: https://gatsby.dev/env-vars
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
         accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
       },
     },
