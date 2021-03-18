bee5891db77d4a28030460d8490812691dae6596
Edward Baldacchino <edward.baldacchino@live.vu.edu.au>
2021-03-18 13:01:25 +1100

    Added Facebook Card meta tags

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -1,6 +1,8 @@
 import React from "react"
 import { Helmet } from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
+import Image from "../assets/twitter-img.png"
+import { useLocation } from "@reach/router"
 
 const query = graphql`
   {
@@ -17,6 +19,7 @@ const query = graphql`
   }
 `
 const SEO = ({ title, description }) => {
+  const { pathname } = useLocation()
   const {
     site: {
       siteMetadata: {
@@ -28,17 +31,36 @@ const SEO = ({ title, description }) => {
         author,
       },
     },
-  } = useStaticQuery(query) 
+  } = useStaticQuery(query)
+
+  const seo = {
+    title: title
+      ? `${title} | ${siteTitle}`
+      : `${siteTitle} | ${siteDescription}`,
+    description: description || siteDescription,
+    image: `${siteUrl}${Image || image}`,
+    url: `${siteUrl}${pathname}`,
+  } 
   return (
-    <Helmet htmlAttributes={{ lang: "en" }} title={`${title} | ${siteTitle}`}>
-      <meta name="description" content={description || siteDescription} />
-      <meta name="image" content={image} />
+    <Helmet htmlAttributes={{ lang: "en" }} title={seo.title}>
+      <meta name="description" content={seo.description} />
+      <meta name="image" content={seo.image} />
       <meta name="author" content={author} />
+      <meta property="og:title" content={siteTitle} />
+      <meta
+        property="og:description"
+        content={description || siteDescription}
+      />
+      <meta property="og:image" content={seo.image} />
+      <meta property="og:url" content={seo.url} />
+      <meta property="og:type" content="website" />
+      <meta property="og:site_name" content={seo.title} />
+      <meta name="twitter:image:alt" content={`${siteTitle} Banner`} />
       <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:creator" content={twitterUsername} />
-      <meta name="twitter:title" content={siteTitle} />
-      <meta name="twitter:description" content={siteDescription} />
-      <meta name="twitter:image" content={`${siteUrl}${image}`} />
+      {/* <meta name='twitter:creator' content={twitterUsername} /> */}
+      <meta name="twitter:title" content={seo.title} />
+      <meta name="twitter:description" content={seo.description} />
+      <meta name="twitter:image" content={seo.image} />
     </Helmet>
   )
 }
