95b2080de234224d9af46f67026097b11de53528
Edward Baldacchino <edwardjbaldacchino@gmail.com>
2023-09-29 06:45:07 +1000

    Now only rendering `<Sidebar />` when toggled open
    - Before, when using the keyboard to focus on elements, I was focusing
      on the navlinks when they were out of view

--- src/components/Layout/Sidebar.tsx
+++ src/components/Layout/Sidebar.tsx
@@ -8,7 +8,40 @@ interface SidebarProps {
   toggleSidebar: () => void
 }
 
-const Sidebar = ({ isOpen, toggleSidebar }: SidebarProps) => {
+const TRANSITION_DURATION_MILLISECONDS = 300
+
+const Sidebar = ({ toggleSidebar, ...props }: SidebarProps) => {
+  let timer = React.useRef<NodeJS.Timeout | null>(null)
+  const [isOpenInternal, setIsOpenInternal] = React.useState(false)
+
+  React.useEffect(() => {
+    if (props.isOpen) {
+      setIsOpenInternal(true)
+    } else {
+      close()
+    }
+
+    return () => {
+      if (timer.current) {
+        clearTimeout(timer.current)
+      }
+    }
+  }, [props.isOpen])
+
+  function close() {
+    timer.current = setTimeout(() => {
+      setIsOpenInternal(false)
+    }, TRANSITION_DURATION_MILLISECONDS)
+  }
+
+  const isRendered = isOpenInternal || props.isOpen
+
+  if (!isRendered) {
+    return null
+  }
+
+  const isOpen = props.isOpen && isOpenInternal
+
   return (
     <aside className={`sidebar ${isOpen ? "show-sidebar" : ""}`}>
       <button onClick={toggleSidebar} className="close-btn">
